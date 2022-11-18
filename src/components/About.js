73338ef76a0a6753fe615a1aa704fef42943aa43
Luna Seifert <thepuddingdeity@gmail.com>
2022-11-18 01:04:49 -0700

    commit tree c294f5e2399415eaebbdb0e82912b6fec4c198dc

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,21 @@
 import React from "react";
+import Links from './Links'
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin}/>
     </div>
   );
 }
 
 export default About;
+
+/*Update the About component so that:
+
+If a prop of bio is passed to this component, it is displayed inside of a <p> tag
+If a prop of bio is not passed down, or if bio is an empty string, don't display the <p> tag
+Hint: you'll need to do some conditional rendering Links to an external site.here to determine whether or not to display the <p> tag*/
